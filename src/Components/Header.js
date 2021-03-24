3ed6fe204c8fc43d32e0f004bde32548abb26543
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-23 22:56:17 -0400

    wrap person and forum icon with icon button to get a click-ripple effect

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,18 +2,24 @@ import React from "react";
 import "../Styles/Header.css";
 import PersonIcon from "@material-ui/icons/Person";
 import ForumIcon from "@material-ui/icons/Forum";
+import IconButton from "@material-ui/core/IconButton";
 
 function Header() {
   return (
     // BEM <<<<<<
     <div className="header">
-      <PersonIcon className="header__icon" fontSize="large" />
+      <IconButton>
+        <PersonIcon className="header__icon" fontSize="large" />
+      </IconButton>
+
       <img
         className="header__logo"
         src="https://cdn.designrush.com/uploads/inspirations/2354/crop_683_410__1513706350_604_tin.png"
         alt="tinder logo"
       />
-      <ForumIcon className="header__icon" fontSize="large" />
+      <IconButton>
+        <ForumIcon className="header__icon" fontSize="large" />
+      </IconButton>
     </div>
   );
 }
