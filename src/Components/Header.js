b75f4c561570c408690df072bb5fd85e6da246ea
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-23 21:15:09 -0400

    Basic format of a very basic header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "../Styles/Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
 
 function Header() {
   return (
-    <div>
+    // BEM <<<<<<
+    <div className="header">
+      <PersonIcon />
       <h2>I am a Header</h2>
+      <ForumIcon />
     </div>
   );
 }
