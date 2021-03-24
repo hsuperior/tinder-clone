1b62509313e3ac0d221596ed0028f748c90370a3
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-23 23:28:59 -0400

    remove start placeholder

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Building Tinder </h1>
-
-      {/** Header */}
       <Header />
       {/** Tinder Cards */}
+
       {/** Buttons below Tinder cards */}
 
       {/** Chats screen */}
