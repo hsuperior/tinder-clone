55094430c8aed1d4f42e25eb66e22a5de28f1c94
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-24 20:31:36 -0400

    playing around with router

--- src/App.js
+++ src/App.js
@@ -6,8 +6,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
-        <Header />
+        <Switch>
+          <Route path="/chat">
+            <h1>I am chatpage</h1>
+          </Route>
+          <Route path="/wilson">
+            <h1>hi wilson</h1>
+          </Route>
+          <Route path="/">
+            <h1>I am homepage</h1>
+          </Route>
+        </Switch>
         {/** Tinder Cards */}
         {/** Buttons below Tinder cards */}
         {/** Chats screen */}
