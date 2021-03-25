329c2cb567bac2fe2144beef9c72a02854871253
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-25 19:58:21 -0400

    install firebase for the project + setup the firebase config and database

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD6nwVcpJXf_IZMaU4WorFH8usb_QCkvKk",
   authDomain: "tinder-clone-74647.firebaseapp.com",
@@ -7,3 +9,9 @@ const firebaseConfig = {
   appId: "1:689565479079:web:c328177ba0d73c55a36b82",
   measurementId: "G-HL1DHKDFMF",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+// firestore is the real-time db provided by firebase
+const database = firebaseApp.firestore();
+
+export default database;
