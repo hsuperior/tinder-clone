a19d08ecf494e208a522c2fc4fa1b5414005576f
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-24 20:24:55 -0400

    import react-router and wrap everything in App with Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Header from "./Components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      {/** Tinder Cards */}
-
-      {/** Buttons below Tinder cards */}
-
-      {/** Chats screen */}
-      {/** Individual chat screen */}
+      <Router>
+        <Header />
+        {/** Tinder Cards */}
+        {/** Buttons below Tinder cards */}
+        {/** Chats screen */}
+        {/** Individual chat screen */}
+      </Router>
     </div>
   );
 }
