97751d498e32a14203d800d1c47538e797d7ca52
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-25 19:46:24 -0400

    add in firebase config file to enable backend capability

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD6nwVcpJXf_IZMaU4WorFH8usb_QCkvKk",
+  authDomain: "tinder-clone-74647.firebaseapp.com",
+  projectId: "tinder-clone-74647",
+  storageBucket: "tinder-clone-74647.appspot.com",
+  messagingSenderId: "689565479079",
+  appId: "1:689565479079:web:c328177ba0d73c55a36b82",
+  measurementId: "G-HL1DHKDFMF",
+};
