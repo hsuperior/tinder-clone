26b4142aeb0df331d527912fccfdc62ac1221fc1
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-24 20:38:11 -0400

    route placeholders for home and chat

--- src/App.js
+++ src/App.js
@@ -12,9 +12,6 @@ function App() {
           <Route path="/chat">
             <h1>I am chatpage</h1>
           </Route>
-          <Route path="/wilson">
-            <h1>hi wilson</h1>
-          </Route>
           <Route path="/">
             <h1>I am homepage</h1>
           </Route>
