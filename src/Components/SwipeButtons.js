359a3a900012913789b5f8aba495c7aca9ef6090
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-27 00:11:50 -0400

    make the swipe buttons clickable by wrapping each icon with material-ui IconButton

--- src/Components/SwipeButtons.js
+++ src/Components/SwipeButtons.js
@@ -4,17 +4,27 @@ import CloseIcon from "@material-ui/icons/Close";
 import StarRateIcon from "@material-ui/icons/StarRate";
 import FavproteIcon from "@material-ui/icons/Favorite";
 import FlashOnIcon from "@material-ui/icons/FlashOn";
-
+import IconButton from "@material-ui/core/IconButton";
 import "../Styles/SwipeButtons.css";
 
 const SwipeButtons = () => {
   return (
     <div className="swipeButtons">
-      <ReplayIcon fontSize="large" />
-      <CloseIcon fontSize="large" />
-      <StarRateIcon fontSize="large" />
-      <FavproteIcon fontSize="large" />
-      <FlashOnIcon fontSize="large" />
+      <IconButton>
+        <ReplayIcon fontSize="large" />
+      </IconButton>
+      <IconButton>
+        <CloseIcon fontSize="large" />
+      </IconButton>
+      <IconButton>
+        <StarRateIcon fontSize="large" />
+      </IconButton>
+      <IconButton>
+        <FavproteIcon fontSize="large" />
+      </IconButton>
+      <IconButton>
+        <FlashOnIcon fontSize="large" />
+      </IconButton>
     </div>
   );
 };
