32309eb08523e26938e227461873942c672ab9ee
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-27 00:06:22 -0400

    start using the Icons for the tinder buttons using material-ui

--- src/Components/SwipeButtons.js
+++ src/Components/SwipeButtons.js
@@ -1,9 +1,20 @@
 import React from "react";
+import ReplayIcon from "@material-ui/icons/Replay";
+import CloseIcon from "@material-ui/icons/Close";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import FavproteIcon from "@material-ui/icons/Favorite";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+
+import "../Styles/SwipeButtons.css";
 
 const SwipeButtons = () => {
   return (
-    <div>
-      <h1>Swipe Buttons</h1>
+    <div className="swipeButtons">
+      <ReplayIcon fontSize="large" />
+      <CloseIcon fontSize="large" />
+      <StarRateIcon fontSize="large" />
+      <FavproteIcon fontSize="large" />
+      <FlashOnIcon fontSize="large" />
     </div>
   );
 };
