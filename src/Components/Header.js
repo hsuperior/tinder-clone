e36a2a59a59997b9aca57ce3ac8508b4eeaed057
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-27 11:13:48 -0400

    render the arrow back icon button if backButton prop gets passed in, else just render the person button like normal

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,15 +3,22 @@ import "../Styles/Header.css";
 import PersonIcon from "@material-ui/icons/Person";
 import ForumIcon from "@material-ui/icons/Forum";
 import IconButton from "@material-ui/core/IconButton";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import { Link } from "react-router-dom";
 
-function Header() {
+function Header({ backButton }) {
   return (
     // BEM <<<<<<
     <div className="header">
-      <IconButton>
-        <PersonIcon className="header__icon" fontSize="large" />
-      </IconButton>
+      {backButton ? (
+        <IconButton>
+          <ArrowBackIosIcon fontSize="large" className="header__icon" />
+        </IconButton>
+      ) : (
+        <IconButton>
+          <PersonIcon className="header__icon" fontSize="large" />
+        </IconButton>
+      )}
 
       <Link to="/">
         <img
