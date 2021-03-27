effe6bd554ce0c46154be1a1b4d24df38e699fa7
Wilson Hsu <hsu.wilson453@gmail.com>
2021-03-27 10:33:13 -0400

    added styling for each specific swipe button

--- src/Components/SwipeButtons.js
+++ src/Components/SwipeButtons.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReplayIcon from "@material-ui/icons/Replay";
 import CloseIcon from "@material-ui/icons/Close";
 import StarRateIcon from "@material-ui/icons/StarRate";
-import FavproteIcon from "@material-ui/icons/Favorite";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 import FlashOnIcon from "@material-ui/icons/FlashOn";
 import IconButton from "@material-ui/core/IconButton";
 import "../Styles/SwipeButtons.css";
@@ -10,19 +10,19 @@ import "../Styles/SwipeButtons.css";
 const SwipeButtons = () => {
   return (
     <div className="swipeButtons">
-      <IconButton>
+      <IconButton className="swipeButtons__repeat">
         <ReplayIcon fontSize="large" />
       </IconButton>
-      <IconButton>
+      <IconButton className="swipeButtons__left">
         <CloseIcon fontSize="large" />
       </IconButton>
-      <IconButton>
+      <IconButton className="swipeButtons__star">
         <StarRateIcon fontSize="large" />
       </IconButton>
-      <IconButton>
-        <FavproteIcon fontSize="large" />
+      <IconButton className="swipeButtons__right">
+        <FavoriteIcon fontSize="large" />
       </IconButton>
-      <IconButton>
+      <IconButton className="swipeButtons__lightning">
         <FlashOnIcon fontSize="large" />
       </IconButton>
     </div>
